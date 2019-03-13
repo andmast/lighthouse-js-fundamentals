81324e92deeab5ac2396574f246fe43f7251d345
Andrea <anmastrantoni@gmail.com>
2019-03-13 13:52:01 +0000

    Adds conditionals exercises

--- go-outside.js
+++ go-outside.js
@@ -14,4 +14,19 @@ if (temperature < 0) {
 } else {
   console.log("Short sleeves are fine.");
 }
+
 console.log("Now you're ready to go outside!");
+
+var isCitizen = true;
+var age = 18
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
