d59c017763547ae51e21d82f4cd7dc0fb8833df4
Andrea <anmastrantoni@gmail.com>
2019-03-13 22:07:16 +0000

    range loop excerise

---
+++ range.js
@@ -0,0 +1,21 @@
+function range(start, end, step) {
+  var walk = [];
+  if (start < end && step >= 0 &&  start !== undefined &&  end !== undefined && step !== undefined) {
+    for (var i = start; i <= end; i += step) {
+      walk.push(i);
+    }
+    return walk;
+  } else {
+    return [];
+  }
+}
+
+
+console.log("answers1")
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log("answers2")
+console.log(range(1,12,"a"));
+console.log(range(0, 8, 2));
+console.log(range(-9, 3, 3));
